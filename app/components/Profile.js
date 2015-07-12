35a773c2f1a0130b4c75e5933fea556638822bf2
Hidenari Nozaki <hidenari@Hidenaris-MBP.fritz.box>
2015-07-12 21:52:27 +1200

    Using ReactFire to Add Data Persistence

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,17 +1,27 @@
 var React = require('react');
 var Router = require('react-router');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
 var Repos = require('./Github/Repos');
 var UserProfile = require('./Github/UserProfile');
 var Notes = require('./Notes/Notes');
 
 var Profile = React.createClass({
-  mixins: [Router.State],
+  mixins: [Router.State, ReactFireMixin],
   getInitialState: function() {
     return {
       notes: ['notes1', 'notes2'],
       bio: {name: 'ghiden'},
       repos: ['angucomplete-alt']
     }
+  },
+  componentDidMount: function() {
+    this.ref = new Firebase('https://github-note-taker.firebaseio.com');
+    var childRef = this.ref.child(this.getParams().username);
+    this.bindAsArray(childRef, 'notes');
+  },
+  componentWillUnmount: function() {
+    this.unbind('notes');
   },
   render: function() {
     var username = this.getParams().username;
