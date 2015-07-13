b322070f27da0db11a70c2e74748063fcae1f41e
Hidenari Nozaki <hidenari@gmail.com>
2015-07-13 21:15:36 +1200

    Component Validation with PropTypes

--- app/components/Github/Repos.js
+++ app/components/Github/Repos.js
@@ -1,9 +1,15 @@
 var React = require('react');
 
 var Repos = React.createClass({
+  propTypes: {
+    username: React.PropTypes.string.isRequired,
+    repos: React.PropTypes.array.isRequired
+  },
   render: function() {
     return (
-      <div> REPOS: {this.props.repos} </div>
+      <div> REPOS: <br/>
+        Username: {this.props.username} <br />
+        Repos: {this.props.repos} </div>
     );
   }
 });
